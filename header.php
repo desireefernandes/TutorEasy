1e0167096ac2574c0f7edfcc6c481519d6cd17e1
Gleiciane Galdino <gleicianegaldino25@gmail.com>
2021-11-05 18:51:36 -0300

    imagens

--- header.php
+++ header.php
@@ -2,18 +2,15 @@
 	<h1 class="header item"><a href="#" title="TutorEasy">TutorEasy</a></h1>
 	<nav>
 		<ul>
-			<li><a href="#" title="inicio">Início</a></li>
-			<li><a href="#" title="tutoriais">Tutoriais</a></li>
+			<li><a href="index.php" title="inicio">Início</a></li>
+			<li><a href="tutoriais.php" title="tutoriais">Tutoriais</a></li>
 			<li><a href="#" title="ranking">Ranking</a></li>
-			<li><a href="#" title="quemsomos">Quem somos?</a></li>
+			<li><a href="quemsomos.php" title="quemsomos">Quem somos?</a></li>
 		</ul>
 	</nav>
 	<div class="right menu">
 		<div class="item">
-			<a class="ui teal basic button">Entrar</a>
-		</div>
-		<div class="item">
-			<a class="ui teal button">Cadastrar-se</a>
+			<a href="login.php" class="ui teal basic button">Entrar</a>
 		</div>
 	</div>
 </header>
--- index.php
+++ index.php
@@ -20,54 +20,62 @@
     <!-- navbar -->
     <?php include 'header.php'; ?>
 
-    <!--Conteúdo index -->
+
+    <!--Conteúdo index-->
     <div class="ui vertical stripe segment">
         <div class="ui middle aligned stackable grid container">
             <div class="row">
                 <div class="eight wide column">
-                    <h3 class="ui header">Estamos aqui para te ajudar</h3>
-                    <p>Está é uma plataforma online que disponibiliza tutoriais de fácil entendimento para pessoas que não possuem vivência com a tecnologia, visando desenvolver a independência desse público principalmente em atividades diárias como: Pagar contas, transferências financeiras ou uma simples chamada de vídeo.</p>
+                    <h3 class="ui header">Ensinando fácil</h3>
+                    <p> Aqui você encontra modelos simples e ilustrativos de tutoriais para diversas atividades 
+                     facilitando ensinamento e o aprendizado.</p>
+                     
+                
                     <h3 class="ui header">Como funciona</h3>
-                    <p>Os usuários inscritos na plataforma podem contribuir criando novos tutoriais personalizados que atendam as necessidades de pessoas de seu convívio e/ou comunidade. Dessa forma, nosso objetivo é criar uma plataforma gratuita e colaborativa que leve aprendizado ao máximo de pessoas de forma social e inclusiva, criando um mecanismo que ajuda e democratiza o acesso à tecnologia para todos.</p>
+                    <p>Os usuários inscritos na plataforma podem contribuir criando novos tutoriais personalizados que atendam as necessidades de pessoas de seu convívio ou de sua comunidade.</p>
+                    <p>Exemplo de tutorial</P>
+                    <img src="img/manu3.jpeg"  class="ui large bordered rounded image">
+        
+                    <h3 class="ui header">Tenha em mãos qualquer tutorial</h3>
+                    <p>Faça downloads, tire prints ou até mesmo imprima e compartilhe com quem você ama.</p>
+
                 </div>
                 <div class="six wide right floated column">
-                    <img src="img/img01.jpeg" class="ui large bordered rounded image">
+                    <img src="img/id.jpg"  class="ui large bordered rounded image">
                 </div>
             </div>
             <div class="row">
-                <div class="center aligned column">
-                    <a href="#" class="btn">Cadastrar-se</a>
-                </div>
             </div>
         </div>
     </div>
 
 
+
+
+    
     <div class="ui vertical stripe quote segment">
         <div class="ui equal width stackable internally celled grid">
             <div class="center aligned row">
                 <div class="column">
-                    <h3>"Lorem Ipsum"</h3>
-                    <p>"tristique commodo primis purus nisi himenaeos. lacinia fermentum aenean augue libero tortor class habitant"</p>
+                    <h3>Alguma sugestão?</h3>
+                    <p>Entre em contato pelo nosso E-mail: tutoreasy@gmail.com.br</p>
                 </div>
-                <div class="column">
-                    <h3>"Lorem ipsum ligulas mattis, lorem conubia sit ultricies commodo nullam scelerisque ligula."</h3>
-                </div>
+                
             </div>
         </div>
     </div>
 
     <div class="ui vertical stripe segment">
         <div class="ui text container">
-            <h3 class="ui header">Fusce nostra rutrum conubia euismod inceptos</h3>
-            <p>Lorem ipsum ligula lobortis tempor orci ultrices mattis, lorem conubia sit ultricies commodo nullam scelerisque ligula, tristique commodo primis purus nisi himenaeos. lacinia fermentum aenean augue libero tortor class habitant turpis potenti cubilia eros ultricies habitasse, neque commodo orci elementum justo at ac fusce nostra rutrum conubia euismod inceptos, commodo nullam pharetra velit condimentum gravida non etiam cubilia per elementum platea.</p>
-            <a class="ui orange button">Lorem Ipsum</a>
+            <h3 class="ui header">Contribua com nossa plataforma</h3>
+            <p>Qualquer pessoa pode contribuir com nosso banco de tutoriais, eles podem ser criados em um simples papel ou editor de imagem e disponibilizado aqui, O único requisito é que ele seja claro e objetivo já que nosso público alvo são pessoas que não são familiarizadas com dispositivos eletrônicos e tecnologia em geral.</p>
+           <!--  <a class="ui orange button">Lorem Ipsum</a>
             <h4 class="ui horizontal header divider">
-                <a href="#">Lorem Ipsum</a>
-            </h4>
+                <a href="#"></a>
+          </h4>
             <h3 class="ui header">Lorem ipsum ligula lobortis temp</h3>
             <p>Tristique commodo primis purus nisi himenaeos. lacinia fermentum aenean augue libero tortor class habitant turpis potenti cubilia eros ultricies habitasse, neque commodo orci elementum justo at ac fusce nostra rutrum conubi.</p>
-            <a class="ui orange button">Lorem Ipsum</a>
+            <a class="ui orange button">Lorem Ipsum</a>-->
         </div>
     </div>
 

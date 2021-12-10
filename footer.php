f8864c39582b9e47016c4e65a73dab82a9faf769
desireefernandes <desireefernandes00@gmail.com>
2021-12-10 14:17:10 -0300

    #21 Fixando footer na base das pág e organizando CSS do projeto

--- footer.php
+++ footer.php
@@ -1,12 +1,6 @@
-<div class="ui inverted vertical footer segment">
 	<footer>
-		
-		<p>
-			TutorEasy©
-			<a class="icon-git" href="https://github.com/desireefernandes/TutorEasy">
-				<i class="github alternate icon"></i>
-			</a>
-		</p>
-
+		<p>©TutorEasy  </p>
+		<a class="icon-git" href="https://github.com/desireefernandes/TutorEasy">
+			<i class="github alternate icon"></i>
+		</a>
 	</footer>
-</div>

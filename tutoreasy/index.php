c44ecdaa2a3227625cf5db2a2314f41a4305d43e
Gleiciane Galdino <gleicianegaldino25@gmail.com>
2022-01-04 19:06:07 -0300

    #31 Listagem dos tutoriais.

--- tutoreasy/index.php
+++ tutoreasy/index.php
@@ -11,6 +11,12 @@
                 padding: 2em;
                 max-width: 200px;
             }
+                li {
+                border: 1px solid hsl(0, 34%, 60%);
+                padding: 2em;
+                max-width: 400px;
+            }
+
             input, button {
                 display: block;
                 margin-top: .5em;
@@ -34,5 +40,51 @@
         <input type="password" name="password" placeholder=" password">
         <input type="submit">
     </form>
+
+
+<!-- LISTAGEM DE TODOS OS TUTORIAIS CADASTRADOS INDEPENDENTE DO USUÁRIO QUE CADASTROU -->
+
+
+<?php
+
+session_start();
+
+include("banco.php");
+
+?>
+
+<h2>Listagem dos Tutoriais Cadastrados</h2>
+
+
+<ul>
+    <?php
+
+    $listagem = $pdo->prepare("SELECT * FROM tutorial");
+    $listagem->execute();
+    while($lista = $listagem->fetch(PDO::FETCH_ASSOC)): 
+    ?>
+
+<!-- LISTAGEM DOS VALUES USER_ID, TITLE, ARQUIVO, DESCRIPTION E O ID DO TUTORIAL-->
+
+    <li><?php echo $lista['user_id']; ?> - 
+        <?php echo $lista['title']; ?> - 
+        <?php echo $lista['arquivo']; ?> - 
+        <?php echo $lista['description']; ?> -
+        <?php echo $lista['id']; ?>
+        
+    </li>
+
+<?php endwhile; ?>
+
+</ul>
+
+
+
 </body>
 </html>
+
+
+
+
+
+

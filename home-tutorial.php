664202b49f5cfb07148ea437fbbf1e7f0ece76ac
desireefernandes <desireefernandes00@gmail.com>
2022-01-07 02:47:41 -0300

    #32 listando tutoriais

--- home-tutorial.php
+++ home-tutorial.php
@@ -8,15 +8,9 @@ session_start();
 //    exit();
 //}
 
-//********************GG*************listar tutorial***********************************//
-include 'banco.php';
+include("banco.php");
 $pdo = dbConnect();
 
-$stmt =  $pdo->prepare('SELECT * FROM tutorial WHERE idtutorial = ? ' ); //user_id = ? não serve
-$stmt->execute([$_SESSION['user_id']]);
-
-$tutorial = $stmt->fetchAll();
-//*******************GG************listar tutorial************************************//
 ?>
 
 <!DOCTYPE html>
@@ -31,100 +25,61 @@ $tutorial = $stmt->fetchAll();
     <!--Semantic UI -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css">
     <script src="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.js"></script>
+    <style>
+        body {
+            background: #F7F8F9;
+        }
+    </style>
 </head>
 
 <body>
     <!-- navbar -->
     <?php include 'header.php'; ?>
-
-    <div class="ui vertical stripe segment">
-        <div class="ui text container">
-            <div class="ui middle aligned center aligned grid">
-
-                <h1>Você está logado, <?= $_SESSION['user_name']; ?>!</h1>
-
-                <div class="item"><a href="user-delete.php?id=<?=$_SESSION['user_id']?>"> Deletar conta</a></div>
-                <div class="item"><a href="logout.php"> Sair</a></div>
-
-                <!--*************GG**********cadastro tutoriais********************-->
-                <table>
-                    <?php foreach ($tutorial as $tutoriais): ?>
-                    <tr>
-                        <td><?= $tutoriais['title'] ?></td>
-                        <td><?= $tutoriais['description'] ?></td>
-                        <td><a href="tutorial-delete.php?id=<?= $tutorial['id']?>"> Apagar tutorial</a></td>
-                    </tr>
-                    <?php endforeach?>
-                </table>
-
-                <!--*************GG*******formulário pra salvar um novo tutoriais***-->
-                <form action="tutorial-save.php" method="POST">
-                    <h2> Novo tutorial</h2>
-                    <input type="text" name="title" placeholder="Titulo">
-                    <input type="text" name="description" placeholder="Descrição">
-                    <input type="Submit" Value="Salvar">
-                </form>
-                <!--**********************************************************************-->
-
-                <!-- TUTORIAIS -->
-                <section id="tutoriais">
-                    <div class="ui vertical stripe segment">
-                        <div class="ui middle aligned stackable grid container">
-                            <div class="row">
-                                <div class="eight wide column">
-                                    <h3 class="ui header">Tutoriais</h3>
-
-                                    <div class="row">
-                                        <div class="ui card">
-                                            <div class="content">
-                                                <div class="header">Como usar o whatsapp</div>
-                                                <a class="image" href="#">
-                                                    <img src="img/manu3.jpeg" class="ui large bordered rounded image">
-                                                </a>
-                                                <div class="description">
-                                                    <p>Como acessar o Whatsapp</p>
-                                                </div>
-                                            </div>
-                                            <div class="extra content">
-                                                <span class="left floated like"><i class="like icon"></i> Gostar </span>
-                                                <span class="right floated star"><i class="star icon"></i> Favorito
-                                                </span>
-                                            </div>
-                                        </div>
-
-                                        <div class="ui card">
-                                            <div class="content">
-                                                <div class="header">Celular android</div>
-                                                <a class="image" href="#">
-                                                    <img src="img/manu2.jpg" class="ui large bordered rounded image">
-                                                </a>
-                                                <div class="description">
-                                                    <p>Funcionalidades do celular</p>
-                                                </div>
-                                            </div>
-                                            <div class="extra content">
-                                                <span class="left floated like"><i class="like icon"></i> Gostar </span>
-                                                <span class="right floated star"><i class="star icon"></i> Favorito
-                                                </span>
-                                            </div>
-                                            <div class="six wide right floated column">
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                </div>
+    <!-- banner -->
+    <div class="banner">
+        <h1 id="title-banner">
+        <!--TutorEasy<br>
+        Aqui você encontra modelos simples e ilustrativos de<br>
+        tutoriais para diversas atividades facilitando<br>
+        ensinamento e o aprendizado.<br>
+            ⇣
+        </h1> -->
+    </div>
+    <!-- <div class="ui vertical stripe segment"> -->
+        <!-- TUTORIAIS -->
+        <div class="container-tutoriais">
+            <div class="content-title-tutorial">
+                <h1>Tutoriais</h1>
+            </div>
+            <!-- CARDS -->
+            <div class="ui four cards">
+                <?php
+                $listagem = $pdo->prepare("SELECT * FROM tutorial");
+                $listagem->execute();
+                    while($lista = $listagem->fetch(PDO::FETCH_ASSOC)): 
+                ?>
+                    <div class="ui card">
+                        <div class="content">
+                            <div class="header"><?=$lista['title'];?></div>
+                            <a class="image" href="#">
+                                <img src="img/manu3.jpeg" class="ui large bordered rounded image">
+                            </a>
+                            <div class="description">
+                                <p><?=$lista['description'];?></p>
                             </div>
                         </div>
+                        <div class="extra content">
+                                <span class="left floated like"><i class="like icon"></i> Gostei </span>
+                                <span class="right floated star"><i class="star icon"></i> Favoritar </span>
+                        </div>
                     </div>
-                </section>
-
+                <?php endwhile; ?>
             </div>
-        </div>
-    </div>
 
+        </div>
+    <!-- </div> -->
     <!-- footer -->
     <?php include 'footer.php'; ?>
-
 </body>
 
 </html>

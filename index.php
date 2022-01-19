aff5bbc832ae627ba1d364346ff5ee949a6a178f
desireefernandes <desireefernandes00@gmail.com>
2022-01-18 21:01:55 -0300

    #37 Modificação index

--- index.php
+++ index.php
@@ -4,221 +4,186 @@
 <html lang="pt">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TutorEasy</title>
-    <link rel="stylesheet" href="css/style.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>TutorEasy</title>
+  <link rel="icon" type="imagem/png" href="/img/_logo/simbolo.png" />
+  <link rel="stylesheet" href="css/style.css">
 
-    <!--Semantic UI -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.js"></script>
+  <!--Semantic UI -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.js"></script>
 
+  <style>
+    body {
+      background: #F7F8F9;
+    }
+  </style>
 </head>
 
 <body>
-    <!-- navbar -->
-    <?php include 'header.php'; ?>
+  <!-- navbar -->
+  <?php include 'header.php'; ?>
 
-    <!--Conteúdo index - INICIO-->
-    <section id="inicio">
-    <div class="ui vertical stripe segment">
-        <div class="ui middle aligned stackable grid container">
-            <div class="row">
-                <div class="eight wide column">
-                    <h3 class="ui header">Ensinando fácil</h3>
-                    <p> Aqui você encontra modelos simples e ilustrativos de tutoriais para diversas atividades 
-                     facilitando ensinamento e o aprendizado.</p>
-                     
-                
-                    <!--<h3 class="ui header">Como funciona</h3>-->
-                    <p>Os usuários inscritos na plataforma podem contribuir criando novos tutoriais personalizados que atendam as necessidades de pessoas de seu convívio ou de sua comunidade.</p>
-                   <!--  <p>Exemplo de tutorial</P>
-                   <img src="img/manu3.jpeg"  class="ui large bordered rounded image">-->
-        
-                    <h3 class="ui header">Tenha em mãos qualquer tutorial</h3>
-                    <p>Faça downloads, tire prints ou até mesmo imprima e compartilhe com quem você ama.</p>
-
-                </div>
-                <div class="six wide right floated column">
-                    <img src="img/People1.png"  class="ui large bordered rounded image">
-                </div>
-            </div>
-            <div class="row">
-            </div>
-        </div>
-    </div>
-    </section>
-
-
-
-    <!-- TUTORIAIS -->
-<section id= "tutoriais">
-<div class="ui vertical stripe segment">
-        <div class="ui middle aligned stackable grid container">
-            <div class="row">
-                <div class="eight wide column">
-                    <h3 class="ui header">Tutoriais</h3>
-                    
-                    
-
-                    <div class="ui card">
-  <div class="content">
-    <div class="header">Como usar o whatsapp</div>
-    <a class="image" href="#">
-    <img src="img/manu3.jpeg" class="ui large bordered rounded image">
-  </a>
-    <div class="description">
-      <p>como acessar o whatsapp</p>
-
-    </div>
-  </div>
-  <div class="extra content">
-    <span class="left floated like"><i class="like icon"></i> Gostar </span>
-    <span class="right floated star"><i class="star icon"></i> Favorito </span>
-  </div>
-                  </div>
-
-                <div class="ui card">
-  <div class="content">
-    <div class="header">Celular android</div>
-    <a class="image" href="#">
-    <img src="img/manu2.jpg" class="ui large bordered rounded image">
-  </a>
-    <div class="description">
-      <p>Funcionalidades do celular</p>
-
-    </div>
-  </div>
-  <div class="extra content">
-    <span class="left floated like"><i class="like icon"></i> Gostar </span>
-    <span class="right floated star"><i class="star icon"></i> Favorito </span>
-  </div>
-                <div class="six wide right floated column">
-                </div>
-            </div>
-            <div class="row">
-            </div>
-        </div>
-    </div>
-
-</section>
-
-
-<!-- QUEM SOMOS -->
-<section id="quemsomos">
-<div class="ui vertical stripe segment">
-        <div class="ui text container">
-            <h3 class="ui header">Estamos aqui para te ajudar</h3>
-            <p>Está é uma plataforma online que disponibiliza tutoriais de fácil entendimento para pessoas que não possuem vivência com a tecnologia, visando desenvolver a independência desse público principalmente em atividades diárias como: Pagar contas, transferências financeiras ou uma simples chamada de vídeo. Os usuários inscritos na plataforma podem contribuir criando novos tutoriais personalizados que atendam as necessidades de pessoas de seu convívio e/ou comunidade. Dessa forma, nosso objetivo é criar uma plataforma gratuita e colaborativa que leve aprendizado ao máximo de pessoas de forma social e inclusiva, criando um mecanismo que ajuda e democratiza o acesso à tecnologia para todos.</p>
-          <!--  <a class="ui orange button">Lorem Ipsum</a> -->
-            <h4 class="ui horizontal header divider"> 
-                <a href="#inicio">.</a>    
-               <h3 class="ui header">Como funciona</h3>
-                <p>Os usuários inscritos na plataforma podem contribuir criando novos tutoriais personalizados que atendam as necessidades de pessoas de seu convívio e/ou comunidade. Dessa forma, nosso objetivo é criar uma plataforma gratuita e colaborativa que leve aprendizado ao máximo de pessoas de forma social e inclusiva, criando um mecanismo que ajuda e democratiza o acesso à tecnologia para todos.</p>
-          <!-- <a class="ui orange button">Lorem Ipsum</a> -->
-            <h4 class="ui horizontal header divider">
-                <a href="#inicio">.</a>   
-            </h4>
-            <h3 class="ui header">Desenvolvedores</h3>
-
-            <div class="ui items">
-  <div class="item">
-    <a class="ui tiny image">
-    <img src="/img/people2.png">
-    </a>
+  <!--Conteúdo index - INICIO-->
+  <!-- Banner 1-->
+  <section class="sec" id="inicio">
     <div class="content">
-      <p class="header">Aline Martineli</p>
-      <div class="description">
-        <p> is a library scientist living in New York City.</p>
+      <div class="mxw800p">
+        <!-- <h1 class="huge ui header" >Bem-vindo
+          <br>ao TutorEasy</h1> -->
+        <br><br><br><br>
+        <p>Aqui você encontra modelos simples e ilustrativos de<br>
+          tutoriais para diversas atividades facilitando<br>
+          ensinamento e o aprendizado.</p>
+        <?php if(isset($_SESSION['user_id'])): ?>
+        <?php else: ?>
+        <a href="login.php" class="ui teal button huge ui button ui left floated button">Cadastrar-se</a>
+        <?php endif; ?>
       </div>
     </div>
-  </div>
-  <div class="item">
-    <a class="ui tiny image">
-    <img src="/img/people2.png">
-    </a>
-    <div class="content">
-      <p class="header">Ana Beatriz</p>
-      <div class="description">
-      <p> is a library scientist living in New York City.</p>      
-    </div>
-    </div>
-  </div>
-  <div class="item">
-    <a class="ui tiny image">
-    <img src="/img/people2.png">
-    </a>
-    <div class="content">
-      <p class="header">Desiree Fernandes</p>
-      <div class="description">
-      <p> is a library scientist living in New York City.</p>
-          </div>
-    </div>
-  </div>
-  <div class="item">
-    <a class="ui tiny image">
-    <img src="/img/people2.png">
-    </a>
-    <div class="content">
-      <p class="header">Gleiciane Galdino</p>
-      <div class="description">
-      <p> is a library scientist living in New York City.</p>      
-    </div>
-    </div>
-  </div>
-  <div class="item">
-    <a class="ui tiny image">
-    <img src="/img/people2.png">
-    </a>
-    <div class="content">
-      <p class="header">Wirpson Andrade</p>
-      <div class="description">
-      <p> is a library scientist living in New York City.</p>
-          </div>
-    </div>
-  </div>
-</div>
-            <!--<a class="ui orange button">Lorem Ipsum</a>-->
-            <h4 class="ui horizontal header divider">
-               <!-- <a href="#">.</a>  --> 
-        </div>
-    </div>
+  </section>
 
-</section>
+  <!-- Banner 2-->
+  <section class="sec-1" id="participar">
+    <div class="content-1">
+      <div class="mxw800p-1">
+        <!-- <h1 class="huge ui header" >Bem-vindo
+          <br>ao TutorEasy</h1> -->
+        <br><br><br><br>
+        <p class="direita">TutorEasy é uma plataforma online que disponibiliza<br>
+          tutoriais de fácil entendimento para pessoas que não<br>
+          possuem vivência com a tecnologia, visando desenvolver a<br>
+          independência desse público principalmente em atividades<br>
+          diárias como: Pagar contas, transferências financeiras<br>
+          ou uma simples chamada de vídeo. </p>
+        <?php if(isset($_SESSION['user_id'])): ?>
+        <?php else: ?>
+        <a href="login.php" class="ui teal button huge ui button ui right floated button">Participar</a>
+        <?php endif; ?>
+      </div>
+    </div>
+  </section>
 
+  <!-- benner 3-->
+  <section class="sec-3" id="contribuir">
+  </section>
 
+  <!-- banner 4-->
+  <section class="sec-4" id="contribuir1">
+  </section>
 
+  <!-- banner 5-->
+  <section class="sec-5" id="quemsomos">
+  </section>
 
-
-<!-- AINDA SERIA O INDEX (O FIMZINHO)-->
-    <div class="ui vertical stripe quote segment">
-        <div class="ui equal width stackable internally celled grid">
-            <div class="center aligned row">
-                <div class="column">
-                    <h3>Alguma sugestão?</h3>
-                    <p>Entre em contato pelo nosso E-mail: tutoreasy@gmail.com.br</p>
-                </div>
-                
+  <!-- TUTORIAIS -->
+  <h2 class="ui center aligned header">Tutoriais</h2>
+  <section class="cardsTuto" id="tutoriais">
+    <input type="radio" name="position" checked />
+    <input type="radio" name="position" />
+    <input type="radio" name="position" />
+    <input type="radio" name="position" />
+    <input type="radio" name="position" />
+    <main id="carousel">
+      <div class="yyy">
+        <div class="ui card">
+          <div class="content">
+            <div class="header">Como instalar um aplicativo</div>
+            <a class="image" href="#">
+              <img src="img/tutoriais/comoInstalarApp.jpeg" class="ui large bordered rounded image">
+            </a>
+            <div class="description">
+              <p>Tutorial de como usar instalar um app no celular</p>
             </div>
+          </div>
+          <div class="extra content">
+            <span class="left floated like"><i class="like icon"></i> Gostar </span>
+            <span class="right floated star"><i class="star icon"></i> Favorito </span>
+          </div>
         </div>
-    </div>
+      </div>
+      <div class="yyy">
+        <div class="ui card">
+          <div class="content">
+            <div class="header">Como fazer pix</div>
+            <a class="image" href="#">
+              <img src="img/tutoriais/FazerPix.png" class="ui large bordered rounded image">
+            </a>
+            <div class="description">
+              <p>Tutorial rápido de como realizar pix</p>
 
-    <div class="ui vertical stripe segment">
-        <div class="ui text container">
-            <h3 class="ui header">Contribua com nossa plataforma</h3>
-            <p>Qualquer pessoa pode contribuir com nosso banco de tutoriais, eles podem ser criados em um simples papel ou editor de imagem e disponibilizado aqui, O único requisito é que ele seja claro e objetivo já que nosso público alvo são pessoas que não são familiarizadas com dispositivos eletrônicos e tecnologia em geral.</p>
-           <!--  <a class="ui orange button">Lorem Ipsum</a>
-            <h4 class="ui horizontal header divider">
-                <a href="#"></a>
-          </h4>
-            <h3 class="ui header">Lorem ipsum ligula lobortis temp</h3>
-            <p>Tristique commodo primis purus nisi himenaeos. lacinia fermentum aenean augue libero tortor class habitant turpis potenti cubilia eros ultricies habitasse, neque commodo orci elementum justo at ac fusce nostra rutrum conubi.</p>
-            <a class="ui orange button">Lorem Ipsum</a>-->
+            </div>
+          </div>
+          <div class="extra content">
+            <span class="left floated like"><i class="like icon"></i> Gostar </span>
+            <span class="right floated star"><i class="star icon"></i> Favorito </span>
+          </div>
         </div>
-    </div>
+      </div>
+      <div class="yyy">
+        <div class="ui card">
+          <div class="content">
+            <div class="header">Como usar PlayStore</div>
+            <a class="image" href="#">
+              <img src="img/tutoriais/baixar.png" class="ui large bordered rounded image">
+            </a>
+            <div class="description">
+              <p>Ótimo tutorial de como usar PlayStore no celular</p>
 
-    <!-- footer -->
-    <?php include 'footer.php'; ?>
+            </div>
+          </div>
+          <div class="extra content">
+            <span class="left floated like"><i class="like icon"></i> Gostar </span>
+            <span class="right floated star"><i class="star icon"></i> Favorito </span>
+          </div>
+        </div>
+      </div>
+      <div class="yyy">
+        <div class="ui card">
+          <div class="content">
+            <div class="header">Como usar o whatsapp</div>
+            <a class="image" href="#">
+              <img src="img/tutoriais/wpp.png" class="ui large bordered rounded image">
+            </a>
+            <div class="description">
+              <p>Fazer vídeo chamada e muito mais, uma ajudinha!</p>
+
+            </div>
+          </div>
+          <div class="extra content">
+            <span class="left floated like"><i class="like icon"></i> Gostar </span>
+            <span class="right floated star"><i class="star icon"></i> Favorito </span>
+          </div>
+        </div>
+      </div>
+      <div class="yyy">
+        <div class="ui card">
+          <div class="content">
+            <div class="header">Como salvar contato</div>
+            <a class="image" href="#">
+              <img src="img/tutoriais/salvarContato.jpeg" class="ui large bordered rounded image">
+            </a>
+            <div class="description">
+              <p>Bom tutorial rápido de como salvar contato</p>
+
+            </div>
+          </div>
+          <div class="extra content">
+            <span class="left floated like"><i class="like icon"></i> Gostar </span>
+            <span class="right floated star"><i class="star icon"></i> Favorito </span>
+          </div>
+        </div>
+      </div>
+      <main>
+  </section>
+
+  <!-- banner 6-->
+  <section class="sec-6" id="contato">
+  </section>
+
+  <!-- footer -->
+  <?php include 'footer.php'; ?>
 
 </body>
 
